d70d7a3a55d01fa34b0ad04c32cb4101bf416884
Navaneeth <162485789+Navaneeth-V@users.noreply.github.com>
2025-07-16 23:42:56 +0530

    student model

---
+++ src/model/Student.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Student {
+
+    private Integer rollNo;
+
+    private String name;
+
+    private Integer age;
+
+    public Integer getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(Integer rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
